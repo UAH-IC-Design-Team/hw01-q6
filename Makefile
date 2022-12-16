f944d05cd5488cb9c532089fb0c5537eb4a6fd3c
mtseng15 <tseng.micah@gmail.com>
2022-12-16 15:21:30 -0600

    initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+build-image:
+	echo "here is where the image build should go"
+
+run:
+	echo "here is where the run target should go"
---
+++ src/app.py
@@ -0,0 +1,20 @@
+# Creates random files with random data.
+
+import random
+import string
+import os
+
+# number of files to be created
+files = 10
+# size of files
+size = 1024*1024
+
+
+for i in range(files):
+    filename = ''.join([random.choice(string.ascii_letters) for i in range(10)])
+    chars = ''.join([random.choice(string.printable) for i in range(size)])
+    with open("/data/"+filename+'.txt', 'w') as f:
+        f.write(chars)
+
+
+print("\nhello everyone!\n")
